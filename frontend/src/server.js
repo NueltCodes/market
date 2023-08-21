3936f5186e008794e80b9bcb1d71fc1d536cf7b5
NueltCodes <olaniranemmanuelt@gmail.com>
2023-08-21 20:15:32 +0100

    updated the frontend server.js with the deployed backend url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://mernstack-e-store.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://mernstack-e-store.vercel.app/";
