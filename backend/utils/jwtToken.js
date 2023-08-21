37d41476e627bd9efb276b262d6ba95aa5bf0538
NueltCodes <olaniranemmanuelt@gmail.com>
2023-08-21 21:11:12 +0100

    fixed the bug on the jwtToken and shopToken which was a spellCheck error

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     sameSite: "none",
     httpOnly: true,
-    Secure: true,
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -5,8 +5,9 @@ const sendShopToken = (user, statusCode, res) => {
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    sameSite: "none",
     httpOnly: true,
-    Secure: true,
+    secure: true,
   };
 
   res.status(statusCode).cookie("seller_token", token, options).json({
